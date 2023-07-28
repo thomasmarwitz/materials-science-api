7a90eb25682807b0109dad789c8f41c588fe97ec
thomasmarwitz <thomasmarwitz3@gmail.com>
2023-07-28 11:54:46 +0200

    Fix readme and CORS

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Start the server
 
-`uvicorn main:app --reload`
+`uvicorn main:app --host 0.0.0.0 --port 8000`
--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from search.search import sem_search, plain_search
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
+origins = ["*"]
 
 app = FastAPI()
 
